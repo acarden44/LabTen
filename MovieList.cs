086bef0dfc95ef431d4dfac8fda3e93caf39b404
acarden44 <aprilcarden44@gmail.com>
2021-11-06 08:07:00 -0400

    Update

---
+++ MovieList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10Movies
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(string movieTitle, string movieCategory)
+        {
+            title = movieTitle;
+            category = movieCategory;
+        }
+
+        public static void PrintMoviesByType(List<Movie> movieList, string categorySelected)
+        {
+            categorySelected.ToLower();
+            foreach (Movie movie in movieList)
+            {
+                if (categorySelected == movie.category.ToLower() || categorySelected == movie.category)
+                {
+                    Console.WriteLine($"{movie.title}");
+                }
+            }
+        }
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movieList = new List<Movie>
+            {
+                new Movie ("Cinderella", "Animated"),
+                new Movie ("Finding Nemo", "Animated"),
+                new Movie ("Snow White", "Animated"),
+                new Movie ("Toy Story", "Animated"),
+                new Movie ("Toy Story 2", "Animated"),
+                new Movie ("Beautiful Boy", "Drama"),
+                new Movie ("Gone with the Wind", "Drama"),
+                new Movie ("On Golden Pond", "Drama"),
+                new Movie ("Stand by Me", "Drama"),
+                new Movie ("West Side Story", "Drama"),
+                new Movie ("Blade Runner", "Scifi"),
+                new Movie ("Serenity", "Scifi"),
+                new Movie ("Star Wars", "Scifi"),
+                new Movie ("The New Mutants", "Scifi"),
+                new Movie ("The Tomorrow War", "Scifi"),
+                new Movie ("Candyman", "Horror"),
+                new Movie ("Friday the 13th", "Horror"),
+                new Movie ("Halloween", "Horror"),
+                new Movie ("IT the Movie", "Horror"),
+                new Movie ("Nightmare on Elm Street", "Horror"),
+            };
+            Console.WriteLine("Welcome to Movie Phone Online!");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+
+            bool userContinue = true;
+            while (userContinue)
+            {
+                string userInput = GetUserInput("Which genre are you interested in?\nScifi\tDrama\tHorror\tAnimated");
+
+                Console.WriteLine();
+                Movie.PrintMoviesByType(movieList, userInput);
+                Console.WriteLine();
+
+                userContinue = AnotherGenre("Would you like to see another genre? (y/n)", "n", "y");
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static bool AnotherGenre(string message, string yes, string no)
+        {
+            string anothergenre = GetUserInput(message);
+            if (anothergenre == yes)
+            {
+                return false;
+            }
+            else if (anothergenre == no)
+            {
+                return true;
+            }
+            else
+            {
+                return AnotherGenre(message, yes, no);
+            }
+        }
+    }
+}
